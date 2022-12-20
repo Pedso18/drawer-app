3f45fc135a4a3e7dfde708f48b4acea559cf70c3
Pedso1812@gmail.com <pedso1812@gmail.com>
2022-12-19 21:59:18 -0300

    project started

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<View style={styles.mainDrawer}></View>
+			<StatusBar style='auto' />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#ededed",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	mainDrawer: {
+		backgroundColor: "#f6e0a2",
+		position: "absolute",
+		right: "5%",
+		bottom: "5%",
+		width: "50%",
+		height: "20%",
+	},
 });
